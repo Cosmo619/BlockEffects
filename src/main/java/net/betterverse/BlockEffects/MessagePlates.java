8f577d52a5274e8ce977947b3993fb294c54e651
Matt Low <matt-low@darqy.net>
2012-04-25 12:50:23 -0600

    Permission to create MessagePlates

--- src/main/java/net/betterverse/BlockEffects/MessagePlates.java
+++ src/main/java/net/betterverse/BlockEffects/MessagePlates.java
@@ -7,6 +7,7 @@ import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class MessagePlates implements Listener {
@@ -26,7 +27,6 @@ public class MessagePlates implements Listener {
     public void onPlayerInteract(PlayerInteractEvent e) {
         if (!e.getAction().equals(Action.PHYSICAL)) return;
         if (e.getClickedBlock().getType().equals(Material.WOOD_PLATE) || e.getClickedBlock().getType().equals(Material.STONE_PLATE)) {
-            if (!e.getPlayer().hasPermission("blockeffects.messageplate")) return;
             BlockState block = e.getClickedBlock().getRelative(BlockFace.DOWN, 2).getState();
             if (!(block instanceof Sign)) return;
             Sign s = (Sign) block;
@@ -36,4 +36,28 @@ public class MessagePlates implements Listener {
             }
         }
     }
+    
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent e) {
+        if (e.getBlock().getState() instanceof Sign) {
+            Material mat = e.getBlock().getRelative(BlockFace.UP, 2).getType();
+            if (mat.equals(Material.WOOD_PLATE) || mat.equals(Material.STONE_PLATE)) {
+                if (!e.getPlayer().hasPermission("blockeffects.messageplate")) {
+                    e.getPlayer().sendMessage("§cYou can't place a Message Plate sign");
+                    e.setCancelled(true);
+                }
+            }
+        }
+        
+        if (e.getBlock().getType().equals(Material.WOOD_PLATE) || e.getBlock().getType().equals(Material.STONE_PLATE)) {
+            BlockState bs = e.getBlock().getRelative(BlockFace.DOWN, 2).getState();
+            if (bs instanceof Sign) {
+                if (!e.getPlayer().hasPermission("blockeffects.messageplate")) {
+                    e.getPlayer().sendMessage("§cYou can't place a Message Plate plate");
+                    e.setCancelled(true);
+                }
+            }
+        }
+    }
+    
 }
