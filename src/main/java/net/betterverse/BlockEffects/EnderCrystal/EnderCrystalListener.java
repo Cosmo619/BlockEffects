04b2cdebce5794d534ab5f7ebee74ad91a9102e9
NuclearW <incongruency@gmail.com>
2012-06-23 02:16:19 -0400

    Event handler to prevent EnderCrystals from disappearing

--- src/main/java/net/betterverse/BlockEffects/EnderCrystal/EnderCrystalListener.java
+++ src/main/java/net/betterverse/BlockEffects/EnderCrystal/EnderCrystalListener.java
@@ -4,6 +4,7 @@ import org.bukkit.World;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 
 public class EnderCrystalListener implements Listener {
@@ -20,4 +21,15 @@ public class EnderCrystalListener implements Listener {
         }
     }
 
+    @EventHandler
+    public void onEntityDamageEvent(EntityDamageEvent event) {
+        EntityType type = event.getEntityType();
+        if (type != EntityType.ENDER_CRYSTAL) {
+            return;
+        }
+
+        if (event.getEntity().getWorld().getEnvironment() != World.Environment.THE_END) {
+            event.setCancelled(true);
+        }
+    }
 }
